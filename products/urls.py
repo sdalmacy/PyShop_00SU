8319db5dd134d447b58b9cfbe782f16042cfd4f8
sdalmacy <57844121+sdalmacy@users.noreply.github.com>
2025-06-15 13:56:24 -0400

    Add AJAX cart and wishlist functionality

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('new/', views.new)
+    path('new/', views.new),
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('add-to-wishlist/', views.add_to_wishlist, name='add_to_wishlist')
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 from .models import Product
 
 
@@ -13,4 +14,32 @@ def index(request):
 
 def new(request):
     return HttpResponse('Welcome to PyShop New Arrivals')
+
+
+@require_POST
+def add_to_cart(request):
+    product_id = request.POST.get('product_id')
+    if not product_id:
+        return JsonResponse({'success': False, 'message': 'No product id'})
+
+    cart = request.session.get('cart', [])
+    if product_id not in cart:
+        cart.append(product_id)
+        request.session['cart'] = cart
+
+    return JsonResponse({'success': True, 'cart_count': len(cart)})
+
+
+@require_POST
+def add_to_wishlist(request):
+    product_id = request.POST.get('product_id')
+    if not product_id:
+        return JsonResponse({'success': False, 'message': 'No product id'})
+
+    wishlist = request.session.get('wishlist', [])
+    if product_id not in wishlist:
+        wishlist.append(product_id)
+        request.session['wishlist'] = wishlist
+
+    return JsonResponse({'success': True, 'wishlist_count': len(wishlist)})
 
